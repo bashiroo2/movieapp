d7e32843d8245287f2822513e31ddcc014c9756a
The money Fly <130142411+bashiroo2@users.noreply.github.com>
2023-06-13 11:33:10 +0000

    Update 4 files

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RatingStars from 'react-rating-stars-component';
 import { Card } from 'react-bootstrap';
 
 const MovieCard = ({ movie }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsFlipped(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsFlipped(false);
+  };
+
   const renderStars = (rating) => {
     return (
-      <RatingStars
-        count={5}
-        value={rating}
-        size={24}
-        activeColor="#ffd700"
-        edit={false}
-      />
+      <div className="rating-stars">
+        <RatingStars
+          count={5}
+          value={rating}
+          size={24}
+          activeColor="#ffd700"
+          edit={false}
+        />
+      </div>
     );
   };
 
   return (
-    <Card className="movie-card">
-      <Card.Img variant="top" src={movie.posterURL} />
-      <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.description}</Card.Text>
-        {renderStars(movie.rating)}
-      </Card.Body>
-    </Card>
+    <div
+      className={`movie-card ${isFlipped ? 'flipped' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          <Card.Img variant="top" src={movie.posterURL} />
+        </div>
+        <div className="card-back">
+          <Card.Body className='text-back-center'>
+            <Card.Title className='ok'>{movie.title}</Card.Title>
+            <Card.Text>{movie.description}</Card.Text>
+            {renderStars(movie.rating)}
+          </Card.Body>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import MovieCard from './MovieCard';
 import { Row, Col } from 'react-bootstrap';
 
 const MovieList = ({ movies }) => {
   return (
-    <Row className="movie-list">
+    <Row style={{width:200}}className="movie-list">
       {movies.map((movie, index) => (
-        <Col key={index} sm={6} md={4} lg={3}>
+        <Col  key={index} sm={6} md={4} lg={3}>
           <MovieCard movie={movie} />
         </Col>
       ))}
